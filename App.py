abc7f991380b1125286271f9e1fa36127d1157df
frankigabe28 <igabemusabefrank@gmail.com>
2025-10-17 22:57:40 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-with open('model_saving.pkl','rb')as file:
-    model=pickle.load(file)
-
+loaded_model=pickle.load(open('model_saving.pkl','rb'))
 st.title("HOUSE PRICE PREDICTION")
 st.write("Enter the following data")
 SF=st.number_input("Square_Footage",value=0)
@@ -14,4 +12,5 @@ GS=st.selectbox("Garage_Size",[0,1])
 if st.button("predict the price"):
     Input=np.array([[SF,NB,NBR,YB,GS]])
     prediction=model.predict(Input)
+
     st.success(f'{prediction[0]:,.2f} $')
